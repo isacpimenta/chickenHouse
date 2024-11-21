1565aacd8e550167a9f6ad785c04697a0f47f307
Isac Pimenta de Abreu <132321630+isacpimenta@users.noreply.github.com>
2024-11-21 01:13:35 -0300

    slac

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{html,js,ejs}",
-    "./public/**/*.html",
-    "./src/index.js",
-    '!./node_modules/**/*',
+    "./src/**/*.{html,js,ejs}", // Monitorar todos os arquivos HTML, JS e EJS dentro de 'src'
+    './src/index.js',           // Monitorar 'index.js' especificamente
+    '!./node_modules/**/*',     // Ignorar arquivos dentro de 'node_modules'
   ],
   theme: {
-    fontFamily:{
+    fontFamily: {
       'sans': ['Oswald', 'sans-serif'],
     },
     extend: {},
   },
   plugins: [],
 }
-
