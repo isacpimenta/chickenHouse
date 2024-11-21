da00f8834ddc8858ed7ff732727a3cc462b3ffdd
Isac Pimenta de Abreu <132321630+isacpimenta@users.noreply.github.com>
2024-11-21 08:04:28 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  if (req.path === '/output.css') {
+    res.setHeader('Content-Type', 'text/css');
+  }
+  next();
+});
+
+
 // Configuração para dados JSON e URL-encoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
